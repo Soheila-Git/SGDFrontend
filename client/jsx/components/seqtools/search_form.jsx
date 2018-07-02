3a7ae77e445afca969da0a8a2177eb09eb744b7f
sweng66 <sweng@stanford.edu>
2018-07-02 15:14:58 -0700

    fixing the issue for non-ORF features

--- client/jsx/components/seqtools/search_form.jsx
+++ client/jsx/components/seqtools/search_form.jsx
@@ -925,7 +925,7 @@ const GeneSequenceResources = React.createClass({
 			  
                 return (<div style={{ textAlign: "top" }}>
                         <h3>Enter a list of names:</h3>
-			<p>[space-separated standard gene names (and/or ORF and/or SGDID). Example: SIR2 YHR023W<br></br>SGD:S000000001. The maximum gene number for this search is 20. It will take first 20 genes if<br></br> more than 20 are provided.] 
+			<p>[space-separated standard gene names (and/or ORF and/or SGDID). <br></br>Example: SIR2 YHR023W SGD:S000000001. The maximum gene number for this search is 20. It will take first 20 genes if more than 20 are provided.] 
 			<textarea ref='genes' name='genes' onChange={this.onChange} rows='2' cols='50'></textarea></p>
 			<h3><b>If available,</b> add flanking basepairs</h3>
 			<p>Upstream: <input type='text' ref='up' name='up' onChange={this.onChange} size='50'></input>
