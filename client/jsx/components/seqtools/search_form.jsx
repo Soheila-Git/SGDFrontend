d3ba220699cbe577c9eaf6e4b7eb072ab4af3878
sweng66 <sweng@stanford.edu>
2018-07-03 18:07:30 -0700

    fixing the issue for non-ORF features

--- client/jsx/components/seqtools/search_form.jsx
+++ client/jsx/components/seqtools/search_form.jsx
@@ -1308,7 +1308,7 @@ const GeneSequenceResources = React.createClass({
                    links = links + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + nextLink;
              }
 
-             return "<h3><br></br><center><p>Display Gene Sets:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + links + "</p></center></h3>";
+             return "<h3>Display Gene Sets:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + links + "</center></h3>";
             
 	},
 
