30a0e968fc1432aded87d35f1089912ec830089d
sweng66 <sweng@stanford.edu>
2018-07-05 15:35:07 -0700

    separating option 2 & 3 section

--- client/jsx/components/seqtools/search_form.jsx
+++ client/jsx/components/seqtools/search_form.jsx
@@ -1032,7 +1032,7 @@ const GeneSequenceResources = React.createClass({
                        <h3>Type or Paste a: </h3>
 		       { seqtypeNode }
 		       <p>Sequence:
-                       <textarea ref='seq' name='seq' onChange={this.onChange} rows='4' cols='100'></textarea></p>
+                       <textarea ref='seq' name='seq' onChange={this.onChange} rows='3' cols='100'></textarea></p>
                 </div>);    
 
 	},
